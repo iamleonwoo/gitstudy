8490b75a2ae0838301aa2f80f72a9026d68a03c1
leonwoo <iamleonwoo@yeah.net>
2024-03-17 23:05:12 +0800

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: test
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author LeonWoo
+ * @Create 2024/3/17 23:01
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
